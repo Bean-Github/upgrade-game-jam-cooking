a4dd3495d15288e1a97d97de5d78e6dadcdc8296
organicbran <bryanchung987@gmail.com>
2024-11-03 12:59:15 -0500

    Final fixes/build

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -11,9 +11,11 @@ public class CameraMovement : MonoBehaviour
     private void Update()
     {
         Vector2 targetPosition = new Vector2(player.position.x, player.position.y + yOffset);
-        Vector2 dampSpeed = Vector2.zero;
-        Vector2 newPosition = Vector2.SmoothDamp(new Vector2(transform.position.x, transform.position.y),
-            targetPosition, ref dampSpeed, smoothSpeed * Time.deltaTime);
+        //Vector2 dampSpeed = Vector2.zero;
+        //Vector2 newPosition = Vector2.SmoothDamp(new Vector2(transform.position.x, transform.position.y),
+            //targetPosition, ref dampSpeed, smoothSpeed * Time.deltaTime);
+            Vector2 newPosition = Vector2.Lerp(new Vector2(transform.position.x, transform.position.y), targetPosition,
+                smoothSpeed * Time.deltaTime);
         transform.position = new Vector3(newPosition.x, newPosition.y, transform.position.z);
     }
 }
