2fac145bf8a6e874fc54b934a13fa3e849927808
organicbran <bryanchung987@gmail.com>
2024-11-02 23:34:44 -0400

    Decorate

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform player;
+    public float smoothSpeed;
+    public float yOffset;
+    
+    private void Update()
+    {
+        Vector2 targetPosition = new Vector2(player.position.x, player.position.y + yOffset);
+        Vector2 dampSpeed = Vector2.zero;
+        Vector2 newPosition = Vector2.SmoothDamp(new Vector2(transform.position.x, transform.position.y),
+            targetPosition, ref dampSpeed, smoothSpeed * Time.deltaTime);
+        transform.position = new Vector3(newPosition.x, newPosition.y, transform.position.z);
+    }
+}
